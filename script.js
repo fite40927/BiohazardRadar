43800cb342ea02468bd0dff85723d82c507f3c66
Apsara <38597307+fite40927@users.noreply.github.com>
2020-09-22 11:54:44 -0700

    Cleaning

--- script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    load("navbar", "nav.html");
-});
-
-function load(id, filename){
-    $("#" + id).html('<object type="text/html" data="' + filename + '" ></object>');
-};
