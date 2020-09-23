0b6756f9fb7e43fc58466bbdaf057d263cc3dd87
Apsara <38597307+fite40927@users.noreply.github.com>
2020-09-23 16:13:10 -0700

    Retrieve CSV from Github repository

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
-    $("#navbar").load("resources/nav.txt");
+$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        url: "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/09-19-2020.csv",
+        dataType: "text",
+        success: function (csvd) {
+            var items = $.csv.toObjects(csvd);
+            var jsonobject = JSON.stringify(items);
+            console.log(jsonobject);
+        },
+        dataType: "text"
+    });
 });
